26966e5189b3b17154270ca8f40d293215a953a4
joelrealtor <jael.ruvalcaba.contractor@realtor.com>
2025-02-10 13:05:11 -0800

    Initial Commit

---
+++ POCHarleyDavidson/POCHarleyDavidsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCHarleyDavidsonApp.swift
+//  POCHarleyDavidson
+//
+//  Created by JaelWizeline on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct POCHarleyDavidsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
